1a994dfbc1819a08877748c9ff6ee60ae1ae22ca
loxeeev <jokoasyok@gmail.com>
2025-11-24 10:08:47 +0700

    add farcaster.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-
 import '~/app/globals.css';
 import { Providers } from '~/app/providers';
 import { APP_NAME, APP_DESCRIPTION } from '~/lib/constants';
@@ -11,15 +10,25 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="fc:miniapp"
+          content='{
+            "id": "burn-cyt-miniapp",
+            "actionUrl": "https://burn-cyt-miniapp.vercel.app",
+            "splashImageUrl": "https://burn-cyt-miniapp.vercel.app/splash.png",
+            "splashTitle": "Change Your Token",
+            "splashDescription": "Burn your token & get CYT automatically"
+          }'
+        />
+      </head>
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
